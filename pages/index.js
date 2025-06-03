63d2bb5ede3b32993582c44c6c4d7f6a97bce10c
Gabriel Morais <leiteetec@gmail.com>
2025-06-03 16:31:18 -0300

    hotfix: Atualiza responsividade

--- pages/index.js
+++ pages/index.js
@@ -25,9 +25,9 @@ export default function Home() {
     <Head>
       <title>Pokédex</title>
     </Head>
-    <div className="flex min-h-screen items-center justify-center bg-gradient-to-b from-slate-700 to-slate-900 bg-no-repeat bg-center bg-cover" style={{ backgroundImage: 'url(/background.gif)' }}>
-      <div className={`${openPokedex && 'bg-black/65'} h-full w-full absolute duration-1000`}></div>
-      <div className={`w-[40rem] h-[45rem] ${PokeMenu2 && 'ml-[-15rem]'} duration-1000 relative`}>
+    <div className="flex h-screen w-full fixed top-0 left-0 items-center justify-center bg-gradient-to-b from-slate-700 to-slate-900 bg-no-repeat bg-center bg-cover" style={{ backgroundImage: 'url(/background.gif)' }}>
+      <div className={`${openPokedex && 'bg-black/65'} h-full w-full fixed duration-1000 top-0 left-0`}></div>
+      <div className={`w-[40rem] absolute h-[45rem] scale-45 md:scale-65 lg:scale-75 2xl:scale-100 xl:mt-[1rem] ${PokeMenu2 ? 'ml-[-9.5rem] md:ml-[-12rem]' : 'ml-[0rem] md:ml-[3rem]'} duration-1000 relative`}>
 
         <div className={`w-[3.8rem] h-[3.8rem] rounded-full bg-sky-300/40 absolute z-50 top-[2.81rem] left-[5.38rem] ${PokeMenu ? 'opacity-100' : 'opacity-0'} duration-1000 overflow-hidden pointer-events-none`}>
           <div className={`w-full h-full bg-white ${PokeMenu2 ? 'opacity-0' : 'opacity-70'} absolute duration-[4s]`}></div>
@@ -50,7 +50,7 @@ export default function Home() {
           <div className={`w-[3rem] h-[30rem] bg-white rotate-45 ${PokeMenu2 ? 'left-[130%]' : 'left-[-100%]'} top-[-7rem] absolute duration-[2.5s]`}></div>
         </div>
 
-        <Image className={`absolute z-30 ${openPokedex && 'h-[45rem] w-0 ml-[27rem]'} duration-300`} width={500} height={900} alt="" src={'/tamp.webp'} onClick={()=>setOpenPokedex(true)}/>
+        <Image className={`absolute z-30 h-[45rem] ${openPokedex && 'w-0 ml-[27rem]'} duration-300`} width={500} height={900} alt="" src={'/tamp.webp'} onClick={()=>setOpenPokedex(true)}/>
         <Image className={`absolute z-40 h-[45rem] ${!PokeMenu ? 'w-0' : 'w-[31rem]'} ml-[25.75rem] duration-300`} width={500} height={900} alt="" src={'/aba.webp'} onClick={()=>setOpenPokedex(true)}/>
         <Image className="absolute z-20 h-[45rem]" width={500} height={900} alt="" src={'/open.webp'}/>
       </div>      
